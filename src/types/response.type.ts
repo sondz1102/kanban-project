d09fde59d6c8aedd5f6c961d0a3915a12e98d932
PhanMinhSon <sonpm@hivelab.com>
2025-08-28 00:02:10 +0700

    update response type

--- src/types/response.type.ts
+++ src/types/response.type.ts
@@ -1,20 +1,12 @@
+import type { User } from "./user.type";
+
 export interface APIResponse<T> {
   success: string;
   message: string;
   data: T;
 }
 
-export interface AuthResponse {
-  id: number;
-  name: string;
-  emai: string;
-  phone: string;
-  role: string;
-  status: number;
-  avatar: string | null;
-  createdAt: string;
-  updatedAt: string;
-
+export interface AuthResponse extends User {
   accessToken: string;
   refreshToken: string;
 }
